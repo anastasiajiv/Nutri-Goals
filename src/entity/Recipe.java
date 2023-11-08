d7fb781b5ee0b92e3cad7721080cf054f963c158
bhardawa <aarya.bhardawaj@mail.utoronto.ca>
2023-11-08 16:47:14 -0500

    Added additional attribute

--- src/entity/Recipe.java
+++ src/entity/Recipe.java
@@ -1,4 +1,5 @@
 package src.entity;
+import java.util.ArrayList;
 import java.util.HashMap;
 public interface Recipe {
 
@@ -9,5 +10,9 @@ public interface Recipe {
     HashMap<String, Double> getRecipeIngredients(); // Using java.lang DOUBLE -> object
 
     String getRecipeInstructions();
+
+    HashMap<Integer, HashMap<String, ArrayList<String>>> savedRecipes(); // {1: {Breakfeast: [RECIPE], Lunch:[RECIPE]},
+                                                                        // 2:{Breakfeast: [RECIPE], Lunch:[RECIPE]}}
+
 
 }
