5b553106df4ca1c71477ad9997e7136a83d33707
Aarya Bhardawaj <123792360+AaryaB04@users.noreply.github.com>
2023-11-07 21:45:48 -0500

    Revert "Began Implementing Login Use Case"

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,12 +1,17 @@
 package src.app;
 
-import src.interface_adapter.login.LoginViewModel;
-import src.interface_adapter.signup.SignupViewModel;
-import src.interface_adapter.login.ViewManagerModel;
-import src.view.LoginView;
-import src.view.SignupView;
-import src.view.ViewManager;
-
+//import interface_adapter.LoginViewModel;
+//import interface_adapter.SignupViewModel;
+//import interface_adapter.ViewManagerModel;
+//import view.LoginView;
+//import view.SignupView;
+//import view.ViewManager;
+//import interface_adapter.LoginViewModel;
+//import interface_adapter.SignupViewModel;
+//import interface_adapter.ViewManagerModel;
+//import view.LoginView;
+//import view.SignupView;
+//import view.ViewManager;
 
 
 import src.entity.CommonUser;
--- src/interface_adapter/signup/SignupController.java
+++
@@ -1,8 +0,0 @@
-package src.interface_adapter.signup;
-
-import src.use_case.signup.SignupInputBoundary;
-import src.use_case.signup.SignupInputData
-
-public class SignupController {
-
-}
