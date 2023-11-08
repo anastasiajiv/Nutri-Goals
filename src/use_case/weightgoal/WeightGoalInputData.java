8d67aa72d10391efec56df731fbce65c19f13a3b
bhardawa <aarya.bhardawaj@mail.utoronto.ca>
2023-11-07 21:08:47 -0500

    Implemented weightgoal input boundry for weight goal use case

--- src/use_case/weightgoal/WeightGoalInputData.java
+++ src/use_case/weightgoal/WeightGoalInputData.java
@@ -1,4 +1,45 @@
 package src.use_case.weightgoal;
 
 public class WeightGoalInputData {
+
+    private final int userId;
+    private final double userHeight;
+
+    private final double userWeight;
+
+    private final int userAge;
+
+    private final int exerciseLevel;
+
+
+
+
+    public WeightGoalInputData(int userId, double userHeight, double userWeight, int userAge, int exerciseLevel) {
+        this.userId = userId;
+        this.userHeight = userHeight;
+        this.userWeight = userWeight;
+        this.userAge = userAge;
+        this.exerciseLevel = exerciseLevel;
+    }
+
+    int getUserId(){
+        return userId;
+    }
+
+    double getUserHeight(){
+        return userHeight;
+    }
+
+    double getUserWeight(){
+        return userWeight;
+    }
+
+    int getUserAge(){
+        return userAge;
+    }
+
+    int getExerciseLevel(){
+        return exerciseLevel;
+    }
+
 }
