64fcd307412e9d8a63c6854baa898f6051ec9aef
bhardawa <aarya.bhardawaj@mail.utoronto.ca>
2023-11-07 20:25:48 -0500

    Implemented Recipe entity

--- src/entity/Recipe.java
+++ src/entity/Recipe.java
@@ -1,2 +1,13 @@
-package src.entity;public interface Recipe {
+package src.entity;
+import java.util.HashMap;
+public interface Recipe {
+
+    int getRecipeId();
+
+    String getRecipeName();
+
+    HashMap<String, Double> getRecipeIngredients(); // Using java.lang DOUBLE -> object
+
+    String getRecipeInstructions();
+
 }
