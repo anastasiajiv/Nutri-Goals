f02504dce66babe289549b34bef6df9a33204187
bhardawa <aarya.bhardawaj@mail.utoronto.ca>
2023-11-08 18:18:01 -0500

    Fixed recipe type for recipe entity

--- src/entity/Recipe.java
+++ src/entity/Recipe.java
@@ -11,7 +11,17 @@ public interface Recipe {
 
     String getRecipeInstructions();
 
-    HashMap<Integer, HashMap<String, ArrayList<String>>> savedRecipes(); // {1: {Breakfeast: [RECIPE], Lunch:[RECIPE]},
+    HashMap<String, Boolean> recipeType();
+    String getRecipeType();
+
+
+//    String getBreakfeast();
+//
+//    String getLunch();
+//
+//    String getDinner();
+
+   // HashMap<Integer, HashMap<String, ArrayList<String>>> savedRecipes(); // {1: {Breakfeast: [RECIPE], Lunch:[RECIPE]},
                                                                         // 2:{Breakfeast: [RECIPE], Lunch:[RECIPE]}}
 
 
