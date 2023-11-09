7c695a8a10ecba592722f304078a42b492a9c788
bhardawa <aarya.bhardawaj@mail.utoronto.ca>
2023-11-09 02:59:39 -0500

    Implemented commonUser

--- src/entity/CommonUserFactory.java
+++ src/entity/CommonUserFactory.java
@@ -1,4 +1,33 @@
 package src.entity;
 
-public class CommonUserFactory {
+import java.time.LocalDateTime;
+import java.util.HashMap;
+
+public class CommonUserFactory implements UserFactory {
+
+
+    @Override
+    public User create(int userId,
+                       String name,
+                       String password,
+                       LocalDateTime creationTime,
+                       double userHeight,
+                       double userWeight,
+                       int userAge,
+                       int userExerciseLevel,
+                       HashMap<String, Boolean> userRestriction,
+                       HashMap<String, Boolean> weightGoal) {
+
+        return new CommonUser
+                (userId,
+                name,
+                password,
+                creationTime,
+                userHeight,
+                userWeight,
+                userAge,
+                userExerciseLevel,
+                userRestriction,
+                weightGoal);
+    }
 }
