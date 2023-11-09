4e88742653465d5c626deb3373ec23ad3de84d22
bhardawa <aarya.bhardawaj@mail.utoronto.ca>
2023-11-09 02:40:27 -0500

    Implemented user DAO

--- src/data_access/FileUserDataAccessObject.java
+++ src/data_access/FileUserDataAccessObject.java
@@ -2,6 +2,7 @@ package src.data_access;
 
 import java.io.*;
 
+import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.HashMap;
 import java.util.Map;
@@ -33,7 +34,7 @@ public class FileUserDataAccessObject implements SignupUserDataAccessInterface {
         headers.put("weight", 5);
         headers.put("age", 6);
         headers.put("exerciseLvl", 7);
-        headers.put("dietaryRestriction", 8);
+        headers.put("dietaryRestriction1", 8);
         headers.put("maintainWeight", 9);
         headers.put("loseWeight", 10);
         headers.put("gainWeight", 11);
@@ -42,7 +43,69 @@ public class FileUserDataAccessObject implements SignupUserDataAccessInterface {
             setHeaders();
         }
         else {
+            try (BufferedReader reader = new BufferedReader(new FileReader(csvFile))) {
+                String header = reader.readLine();
 
+                assert header.equals("userId," +
+                        "username," +
+                        "password," +
+                        "creationTime," +
+                        "height," +
+                        "weight," +
+                        "age," +
+                        "exerciseLvl," +
+                        "dietaryRestriction1," +
+                        "maintainWeight," +
+                        "loseWeight," +
+                        "gainWeight");
+
+                String row;
+                while ((row = reader.readLine()) != null) {
+                    String[] col = row.split(",");
+                    int userId = Integer.parseInt(col[headers.get("userId")]);
+                    String username = String.valueOf(col[headers.get("username")]);
+                    String password = String.valueOf(col[headers.get("password")]);
+                    String creationTimeText = String.valueOf(col[headers.get("creationTime")]);
+
+                    LocalDateTime ldt = LocalDateTime.parse(creationTimeText);
+
+                    double height = Double.parseDouble(col[headers.get("height")]);
+                    double weight = Double.parseDouble(col[headers.get("weight")]);
+                    int age = Integer.parseInt(col[headers.get("age")]);
+                    int exerciseLvl = Integer.parseInt(col[headers.get("exerciseLvl")]);
+
+                    String restrictionKey1 = "dietaryRestriction1"; // Replace with each type of restriction
+                    Boolean restrictionValue1 = Boolean.valueOf(col[headers.get("dietaryRestriction1")]);
+
+                    HashMap<String, Boolean> restrictions = new HashMap<>();
+                    restrictions.put(restrictionKey1, restrictionValue1);
+
+                    String weightGoalKey1 = "maintainWeight";
+                    Boolean weightGoalValue1 = Boolean.valueOf(col[headers.get(weightGoalKey1)]);
+                    String weightGoalKey2 = "loseWeight";
+                    Boolean weightGoalValue2 = Boolean.valueOf(col[headers.get(weightGoalKey2)]);
+                    String weightGoalKey3 = "gainWeight";
+                    Boolean weightGoalValue3 = Boolean.valueOf(col[headers.get(weightGoalKey3)]);
+
+                    HashMap<String, Boolean> weightGoal = new HashMap<>();
+                    weightGoal.put(weightGoalKey1, weightGoalValue1);
+                    weightGoal.put(weightGoalKey2, weightGoalValue2);
+                    weightGoal.put(weightGoalKey3, weightGoalValue3);
+
+                    User user = userFactory.create(userId,
+                            username,
+                            password,
+                            ldt,
+                            height,
+                            weight,
+                            age,
+                            exerciseLvl,
+                            restrictions,
+                            weightGoal);
+                            accounts.put(userId, user);
+
+                }
+            }
 
         }
 
