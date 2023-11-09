523fe8d3c998826a4181ee9f5cfd973f183e06aa
bhardawa <aarya.bhardawaj@mail.utoronto.ca>
2023-11-09 02:40:49 -0500

    Added userId to constructor

--- src/entity/UserFactory.java
+++ src/entity/UserFactory.java
@@ -4,8 +4,14 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 
 public interface UserFactory {
-     User create(String name, String password, LocalDateTime creationTime, double userHeight, double userWeight,
-                 int userAge, int userExerciseLevel, HashMap<String, Boolean> userRestriction,
+     User create(int userId,
+                 String name,
+                 String password,
+                 LocalDateTime creationTime,
+                 double userHeight,
+                 double userWeight,
+                 int userAge,
+                 int userExerciseLevel,
+                 HashMap<String, Boolean> userRestriction,
                  HashMap<String, Boolean> weightGoal);
-
 }
