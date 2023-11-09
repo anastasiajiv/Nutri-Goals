320545cf484fe175959de252cdc7b53c264a80b2
bhardawa <aarya.bhardawaj@mail.utoronto.ca>
2023-11-08 19:14:23 -0500

    Added user and userId to data interface

--- src/use_case/weightgoal/WeightGoalUserDataInterface.java
+++ src/use_case/weightgoal/WeightGoalUserDataInterface.java
@@ -1,4 +1,10 @@
 package src.use_case.weightgoal;
 
+import src.entity.User;
+
 public interface WeightGoalUserDataInterface {
-}
+    void saveWeightGoalData(User user);
+
+    String getUserWeightGoalData(int userId);
+    }
+
