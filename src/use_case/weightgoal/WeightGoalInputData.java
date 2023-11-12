b0a5f9dcb5892c4dac235466c123a92f647da2f3
Aarya Bhardawaj <aarya.bhardawaj@gmail.com>
2023-11-12 00:44:33 -0500

    Changed input data attributes

--- src/use_case/weightgoal/WeightGoalInputData.java
+++ src/use_case/weightgoal/WeightGoalInputData.java
@@ -1,53 +1,33 @@
 package src.use_case.weightgoal;
 
 import java.util.HashMap;
+import java.util.Map;
 
+import src.entity.User;
+import src.entity.CommonUser;
 public class WeightGoalInputData {
 
-    private final int userId;
-    private final double userHeight;
-
-    private final double userWeight;
-
-    private final int userAge;
-
-    private final int exerciseLevel;
-
-    private HashMap<String, Boolean> restrictions;
-
-    private HashMap<String, Boolean> weightGoalType;
+    private HashMap<Integer, String> weightGoalType;
 
 
+    public WeightGoalInputData(HashMap<Integer, String> weightGoalType){
+        this.weightGoalType = weightGoalType;
+    }
 
+    String getWeightGoalType(){
+        String strWeightGoalType = "";
+        for (Map.Entry<Integer, String> entry: weightGoalType.entrySet()) { // Should only be 1 loop
+            strWeightGoalType = entry.getValue();
+        }
+        return strWeightGoalType;
 
-    public WeightGoalInputData(int userId, double userHeight, double userWeight, int userAge, int exerciseLevel) {
-        this.userId = userId;
-        this.userHeight = userHeight;
-        this.userWeight = userWeight;
-        this.userAge = userAge;
-        this.exerciseLevel = exerciseLevel;
     }
 
     int getUserId(){
+        int userId = 0;
+        for (Map.Entry<Integer, String> entry: weightGoalType.entrySet()) { // Should only be 1 loop
+            userId = entry.getKey();
+        }
         return userId;
     }
-
-    double getUserHeight(){
-        return userHeight;
-    }
-
-    double getUserWeight(){
-        return userWeight;
-    }
-
-    int getUserAge(){
-        return userAge;
-    }
-
-    int getExerciseLevel(){
-        return exerciseLevel;
-    }
-
-
-
 }
