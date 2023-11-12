98dcba35cbb87e1354dcc34591ee7537dcfc08c6
Sophia Li <sophiali@Sophias-MBP.lan>
2023-11-11 23:28:54 -0500

    Implemented input data for nutrients use case

--- src/use_case/nutrients/NutrientsInputData.java
+++ src/use_case/nutrients/NutrientsInputData.java
@@ -1,2 +1,16 @@
-package src.use_case.nutrients;public class NutrientsInputData {
+package src.use_case.nutrients;
+
+import java.util.HashMap;
+
+public class NutrientsInputData {
+    private final int userID;
+    private HashMap<String, Boolean> nutrients;
+
+    public NutrientsInputData(int userID) {
+        this.userID = userID;
+    }
+
+    int getUserID() {
+        return this.userID;
+    }
 }
