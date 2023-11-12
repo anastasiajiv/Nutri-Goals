06713bb518f1b3f2bee2f56c7c16fecc0928bc6f
Sophia Li <sophiali@Sophias-MBP.lan>
2023-11-11 23:31:28 -0500

    Implemented DAO for nutrients use case

--- src/use_case/nutrients/NutrientsUserDataAccessInterface.java
+++ src/use_case/nutrients/NutrientsUserDataAccessInterface.java
@@ -1,2 +1,11 @@
-package src.use_case.nutrients;public interface NutrientsUserDataAccessInterface {
+package src.use_case.nutrients;
+
+import src.entity.User;
+
+public interface NutrientsUserDataAccessInterface {
+    void saveNutrientsData(User user);
+
+    User getUserNutrientsData(int userID);
+
+    Boolean existByUserID(int userID);
 }
