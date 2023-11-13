abd693b29e347cf7d70814a00809addadffd37cb
Sophia Li <sophiali@Sophias-MacBook-Pro.local>
2023-11-13 15:28:42 -0500

    Began outlining methods for nutrients interactor

--- src/use_case/nutrients/NutrientsInteractor.java
+++ src/use_case/nutrients/NutrientsInteractor.java
@@ -1,4 +1,6 @@
 package src.use_case.nutrients;
 
-public class NutrientsInteractor {
+public class NutrientsInteractor implements NutrientsInputBoundary {
+    final NutrientsUserDataAccessInterface userDataAccessObject;  // name?
+    final NutrientsOutputBoundary nutrientsPresenter;
 }
