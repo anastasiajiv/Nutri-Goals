c2cf798465de90a6efc92fd43c810556813bbf1f
Aarya Bhardawaj <aarya.bhardawaj@gmail.com>
2023-11-13 16:04:05 -0500

    Added sample tests for save DAO methods

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,14 +1,86 @@
 package src.app;
 
+//import interface_adapter.LoginViewModel;
+//import interface_adapter.SignupViewModel;
+//import interface_adapter.ViewManagerModel;
+//import view.LoginView;
+//import view.SignupView;
+//import view.ViewManager;
+
+
+import src.data_access.FileUserDataAccessObject;
 import src.entity.CommonRecipe;
 import src.entity.CommonUser;
+import src.entity.*;
+import src.entity.UserFactory;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
+        String csvFilePath = "test.csv";
 
+        UserFactory userFactory = new CommonUserFactory();
+
+        HashMap<String, Boolean> weightgoal = new HashMap<>();
+        weightgoal.put("maintainWeight", Boolean.TRUE);
+
+        try {
+            FileUserDataAccessObject userDAO = new FileUserDataAccessObject(csvFilePath, userFactory);
+            User user = new CommonUser(1,
+                    "Aarya",
+                    "Password",
+                    LocalDateTime.now(),
+                    new HashMap<>(),
+                    175,
+                    65.5,
+                    19,
+                    1,
+                    new HashMap<>(),
+                    new HashMap<>(),
+                    0
+                    );
+
+
+
+            User user2 = new CommonUser(2,
+                    "Aarya",
+                    "Password",
+                    LocalDateTime.now(),
+                    new HashMap<>(),
+                    175,
+                    65.5,
+                    19,
+                    1,
+                    new HashMap<>(),
+                    new HashMap<>(),
+                    0
+                );
+
+
+            User user3 = new CommonUser(1,
+                    "Aarya",
+                    "Password",
+                    LocalDateTime.now(),
+                    new HashMap<>(),
+                    200000000.0,
+                    65.5,
+                    19,
+                    1,
+                    new HashMap<>(),
+                    weightgoal,
+                    0
+            );
+
+            // Optional[String] : None
+            userDAO.saveWeightGoalData(user3);
+            System.out.println(weightgoal);
+            //System.out.println(userDAO.existById(2));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
