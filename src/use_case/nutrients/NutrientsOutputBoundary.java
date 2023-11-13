78aa00b17f653c8177c2128f92844b2efae71c2c
Sophia Li <sophiali@Sophias-MacBook-Pro.local>
2023-11-13 15:25:22 -0500

    Implemented output boundary and data for nutrients use case

--- src/use_case/nutrients/NutrientsOutputBoundary.java
+++ src/use_case/nutrients/NutrientsOutputBoundary.java
@@ -1,4 +1,6 @@
 package src.use_case.nutrients;
 
 public interface NutrientsOutputBoundary {
+    void prepareSuccessView(NutrientsOutputData nutrients);
+    void prepareFailView(String error);
 }
--- src/use_case/nutrients/NutrientsOutputData.java
+++ src/use_case/nutrients/NutrientsOutputData.java
@@ -1,4 +1,16 @@
 package src.use_case.nutrients;
 
 public class NutrientsOutputData {
+    private final int userID;
+    private boolean useCaseFailed;
+
+    public NutrientsOutputData(int userID, boolean useCaseFailed) {
+        this.userID = userID;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public int getUserID() {
+        return this.userID;
+    }
+
 }
