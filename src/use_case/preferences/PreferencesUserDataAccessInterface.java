cf1903e0929af38d6bbd62a27dc647a3ccb57ba3
Anastasia <anastasiajiv13@gmail.com>
2023-11-18 21:08:38 -0500

    implemented the data access object interface for the preferences use-case

--- src/use_case/preferences/PreferencesUserDataAccessInterface.java
+++ src/use_case/preferences/PreferencesUserDataAccessInterface.java
@@ -2,4 +2,10 @@ package src.use_case.preferences;
 import java.util.*;
 public interface PreferencesUserDataAccessInterface {
     // where for each Integer refers to userId
+    void saveDietary(HashMap<Integer, HashMap<String, Boolean>> user_dietery);
+    void saveConditions(HashMap<Integer, HashMap<String, String>> user_condition);
+
+    void saveAllergies(HashMap<Integer, HashMap<String, Boolean>> user_allergies);
+
+    boolean existsByID(int id);
 }
