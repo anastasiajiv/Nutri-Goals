f2833d01403af1eee36d485731f71b8b19ba3334
Anastasia <anastasiajiv13@gmail.com>
2023-11-20 15:51:51 -0500

    added and initialized a userDaataAccessObject and preferencesPresenter attribute

--- src/use_case/preferences/PreferencesInteractor.java
+++ src/use_case/preferences/PreferencesInteractor.java
@@ -1,6 +1,16 @@
 package src.use_case.preferences;
 import src.entity.User;
 import java.util.*;
-public class PreferencesInteractor {
+public class PreferencesInteractor implements PreferencesInputBoundary{
+    final PreferencesUserDataAccessInterface userDataAccessObject;
+    final PreferencesOutputBoundary preferencesPresenter;
+
+    public PreferencesInteractor(PreferencesUserDataAccessInterface userDataAccessInterface,
+                                PreferencesOutputBoundary preferencesOutputBoundary){
+        this.userDataAccessObject = userDataAccessInterface;
+        this.preferencesPresenter = preferencesOutputBoundary;
+    }
+
+
 
 }
