3915ca4c35d8cf5e8a644640e92a525a52067fed
Anastasia <anastasiajiv13@gmail.com>
2023-11-24 18:13:25 -0500

    added comments about changes that should be made to the parameters of each method

--- src/use_case/preferences/PreferencesUserDataAccessInterface.java
+++ src/use_case/preferences/PreferencesUserDataAccessInterface.java
@@ -2,10 +2,11 @@ package src.use_case.preferences;
 import java.util.*;
 public interface PreferencesUserDataAccessInterface {
     // where for each Integer refers to userId
+    // want to change the way that the parameters for each method, UserID(which is an integer should be seperate)
     void saveDietary(HashMap<Integer, HashMap<String, Boolean>> user_dietery);
     void saveConditions(HashMap<Integer, HashMap<String, String>> user_condition);
 
     void saveAllergies(HashMap<Integer, HashMap<String, Boolean>> user_allergies);
 
-    boolean existsByID(int id);
+    boolean existById(int id);
 }
