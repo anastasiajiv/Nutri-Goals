3fd9196b414e6e5f29da3ce9016a1d606afb2791
Anastasia <anastasiajiv13@gmail.com>
2023-11-27 21:44:22 -0500

    initialized and implemented the SignupPresenter

---
+++ src/interface_adapters/signup/SignupPresenter.java
@@ -0,0 +1,46 @@
+package src.interface_adapters.signup;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.ViewManagerModel;
+import use_case.signup.SignupOutputBoundary;
+import use_case.signup.SignupOutputData;
+
+public class SignupPresenter {
+    private final SignupViewModel signupViewModel;
+    private final LoginViewModel loginViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public SignupPresenter(ViewManagerModel viewManagerModel,
+                           SignupViewModel signupViewModel,
+                           LoginViewModel loginViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.signupViewModel = signupViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(SignupOutputData response) {
+        // On success, switch to the login view.
+        LocalDateTime responseTime = LocalDateTime.parse(response.getCreationTime());
+        response.setCreationTime(responseTime.format(DateTimeFormatter.ofPattern("hh:mm:ss")));
+
+        LoginState loginState = loginViewModel.getState();
+        loginState.setUsername(response.getUsername());
+        this.loginViewModel.setState(loginState);
+        loginViewModel.firePropertyChanged();
+
+        viewManagerModel.setActiveView(loginViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        SignupState signupState = signupViewModel.getState();
+        signupState.setUsernameError(error);
+        signupViewModel.firePropertyChanged();
+    }
+
+}
