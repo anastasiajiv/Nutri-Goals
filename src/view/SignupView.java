91f1400140a4a399df3938ef4248e61d56f85744
Anastasia <anastasiajiv13@gmail.com>
2023-12-01 17:51:48 -0500

    added the view for signup and implemented the appropriate buttons for signin

---
+++ src/view/SignupView.java
@@ -0,0 +1,151 @@
+package src.view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import src.interface_adapters.signup.SignupController;
+import src.interface_adapters.signup.SignupState;
+import src.interface_adapters.signup.SignupViewModel;
+public class SignupView extends JPanel implements ActionListener, PropertyChangeListener{
+
+    private final CardLayout cardLayout;
+    private final JPanel views;
+    public final String viewName = "sign up";
+
+    public final SignupViewModel signupViewModel;
+
+    private final JButton signUp;
+
+    private final JButton cancel;
+
+    private final JTextField usernameInputField = new JTextField(15);
+    private final JPasswordField passwordInputField = new JPasswordField(15);
+    private final JPasswordField repeatPasswordInputField = new JPasswordField(15);
+
+    private final SignupController signupController;
+
+    public SignupView(SignupController controller, SignupViewModel signupViewModel, CardLayout cardLayout, JPanel views){
+        this.signupController = controller;
+        this.signupViewModel = signupViewModel;
+        this.cardLayout = cardLayout;
+        this.views = views;
+        signupViewModel.addPropertyChangeListener(this);
+        JLabel title = new JLabel(SignupViewModel.TITLE_LABEL);
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        LabelTextPanel usernameInfo = new LabelTextPanel(
+                new JLabel(SignupViewModel.USERNAME_LABEL), usernameInputField);
+        LabelTextPanel passwordInfo = new LabelTextPanel(
+                new JLabel(SignupViewModel.PASSWORD_LABEL), passwordInputField);
+        LabelTextPanel repeatPasswordInfo = new LabelTextPanel(
+                new JLabel(SignupViewModel.REPEAT_PASSWORD_LABEL), repeatPasswordInputField);
+
+        JPanel buttons = new JPanel();
+        signUp = new JButton(SignupViewModel.SIGNUP_BUTTON_LABEL);
+        buttons.add(signUp);
+        cancel = new JButton(SignupViewModel.CANCEL_BUTTON_LABEL);
+        buttons.add(cancel);
+
+        signUp.addActionListener(
+                new ActionListener(){
+                    public void actionPerformed(ActionEvent evt){
+                        SignupState currentState = signupViewModel.getState();
+
+                        signupController.execute(
+                                currentState.getUsername(),
+                                currentState.getPassword(),
+                                currentState.getPassword()
+                        );
+                        cardLayout.show(views, "log in");
+                    }
+                }
+                );
+
+        cancel.addActionListener(this);
+
+        usernameInputField.addKeyListener(
+                new KeyListener() {
+                    @Override
+                    public void keyTyped(KeyEvent e) {
+                        SignupState currentState = signupViewModel.getState();
+                        String text = usernameInputField.getText() + e.getKeyChar();
+                        currentState.setUsername(text);
+                        signupViewModel.setState(currentState);
+                    }
+
+                    @Override
+                    public void keyPressed(KeyEvent e) {
+                    }
+
+                    @Override
+                    public void keyReleased(KeyEvent e) {
+                    }
+                });
+
+        passwordInputField.addKeyListener(
+                new KeyListener() {
+                    @Override
+                    public void keyTyped(KeyEvent e) {
+                        SignupState currentState = signupViewModel.getState();
+                        currentState.setPassword(passwordInputField.getText() + e.getKeyChar());
+                        signupViewModel.setState(currentState);
+                    }
+
+                    @Override
+                    public void keyPressed(KeyEvent e) {
+
+                    }
+
+                    @Override
+                    public void keyReleased(KeyEvent e) {
+
+                    }
+                }
+        );
+
+        repeatPasswordInputField.addKeyListener(
+                new KeyListener() {
+                    @Override
+                    public void keyTyped(KeyEvent e) {
+                        SignupState currentState = signupViewModel.getState();
+                        currentState.setRepeatPassword(repeatPasswordInputField.getText() + e.getKeyChar());
+                        signupViewModel.setState(currentState); // Hmm, is this necessary?
+                    }
+
+                    @Override
+                    public void keyPressed(KeyEvent e) {
+
+                    }
+
+                    @Override
+                    public void keyReleased(KeyEvent e) {
+
+                    }
+                }
+        );
+    this.add(title);
+    this.add(usernameInfo);
+    this.add(passwordInfo);
+    this.add(repeatPasswordInfo);
+    this.add(buttons);
+
+    }
+
+    public void actionPerformed(ActionEvent evt) {
+        JOptionPane.showConfirmDialog(this, "Cancel not implemented yet.");
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        SignupState state = (SignupState) evt.getNewValue();
+        if (state.getUsernameError() != null) {
+            JOptionPane.showMessageDialog(this, state.getUsernameError());
+        }
+    }
+
+}
