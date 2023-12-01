b46f8e38c461376a41beca14a98859113f821707
Anastasia <anastasiajiv13@gmail.com>
2023-12-01 14:01:36 -0500

    initialized the viewmodel and implemented the getState method

--- src/interface_adapters/preferences/PreferencesViewModel.java
+++ src/interface_adapters/preferences/PreferencesViewModel.java
@@ -1,6 +1,10 @@
 package src.interface_adapters.preferences;
 
 import src.interface_adapters.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class PreferencesViewModel extends ViewModel{
     public static final String TITLE_LABEL = "Preferences View";
     public static final String DIETARY_LABEL = "Select Dietary Option";
@@ -8,4 +12,24 @@ public class PreferencesViewModel extends ViewModel{
     public static final String CONDITIONS_LABEL = "Select Amount for a Specific Nutrient";
 
     public static final String PREFERENCES_BUTTON_LABEL = "preferences";
+
+    private PreferencesState state = new PreferencesState();
+
+    public PreferencesViewModel(){super("preferences");}
+
+    public void setState(PreferencesState state) {this.state = state;}
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public PreferencesState getState() {
+        return state;
+    }
 }
