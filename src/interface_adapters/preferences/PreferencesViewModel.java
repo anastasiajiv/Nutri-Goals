92c30b2d46f567163f1c25bbe2492651fdaa43e5
Anastasia <anastasiajiv13@gmail.com>
2023-12-01 13:58:35 -0500

    added appropriate labels for preferences

--- src/interface_adapters/preferences/PreferencesViewModel.java
+++ src/interface_adapters/preferences/PreferencesViewModel.java
@@ -1,4 +1,11 @@
 package src.interface_adapters.preferences;
 
-public class PreferencesViewModel {
+import src.interface_adapters.ViewModel;
+public class PreferencesViewModel extends ViewModel{
+    public static final String TITLE_LABEL = "Preferences View";
+    public static final String DIETARY_LABEL = "Select Dietary Option";
+    public static final String ALLERGIES_LABEL = "Select Any Allergies You Have";
+    public static final String CONDITIONS_LABEL = "Select Amount for a Specific Nutrient";
+
+    public static final String PREFERENCES_BUTTON_LABEL = "preferences";
 }
