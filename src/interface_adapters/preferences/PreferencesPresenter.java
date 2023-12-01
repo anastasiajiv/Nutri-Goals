2249699c797ad683be460cd1d93863e3742dbedf
Anastasia <anastasiajiv13@gmail.com>
2023-12-01 00:35:53 -0500

    added attributes to Presenter

--- src/interface_adapters/preferences/PreferencesPresenter.java
+++ src/interface_adapters/preferences/PreferencesPresenter.java
@@ -1,4 +1,10 @@
 package src.interface_adapters.preferences;
 
-public class PreferencesPresenter {
+import src.interface_adapters.ViewManagerModel;
+import src.use_case.preferences.PreferencesOutputBoundary;
+
+public class PreferencesPresenter implements PreferencesOutputBoundary {
+    private final PreferencesViewModel preferencesViewModel;
+
+    private ViewManagerModel viewManagerModel;
 }
