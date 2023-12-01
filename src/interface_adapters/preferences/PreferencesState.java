3450ed5e11aef1aac9d521550fc97f881892ac9b
Anastasia <anastasiajiv13@gmail.com>
2023-12-01 13:53:30 -0500

    created copy of state and added methods for retrieving attributes

--- src/interface_adapters/preferences/PreferencesState.java
+++ src/interface_adapters/preferences/PreferencesState.java
@@ -5,5 +5,16 @@ public class PreferencesState {
     private HashMap<String, Boolean> allergies = null;
     private HashMap<String, String> conditions = null;
 
+    public PreferencesState(PreferencesState copy){
+        dietary = copy.dietary;
+        allergies = copy.allergies;
+        conditions = copy.conditions;
+    }
+
+    public PreferencesState(){}
+
+    public HashMap<String, Boolean> getDietaryMap(){return dietary;}
+    public HashMap<String, Boolean> getAllergiesMap(){return allergies;}
+    public HashMap<String, String> getConditionsMap(){return conditions;}
 
 }
