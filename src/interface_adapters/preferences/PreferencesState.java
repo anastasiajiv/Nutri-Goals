d9a1682f87d98d80b56c3c9c856f9296d0be6455
Anastasia <anastasiajiv13@gmail.com>
2023-12-01 13:47:08 -0500

    added the appropriate attributes to the preferences state class

--- src/interface_adapters/preferences/PreferencesState.java
+++ src/interface_adapters/preferences/PreferencesState.java
@@ -1,4 +1,9 @@
 package src.interface_adapters.preferences;
-
+import java.util.*;
 public class PreferencesState {
+    private HashMap<String, Boolean> dietary = null;
+    private HashMap<String, Boolean> allergies = null;
+    private HashMap<String, String> conditions = null;
+
+
 }
