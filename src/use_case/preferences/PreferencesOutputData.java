1941de3fa8be466f73aa69a433f8b3a58817fda4
Anastasia <anastasiajiv13@gmail.com>
2023-12-01 00:32:17 -0500

    implemented the Output Data class for preferences with methods to return to the user their choices

--- src/use_case/preferences/PreferencesOutputData.java
+++ src/use_case/preferences/PreferencesOutputData.java
@@ -1,5 +1,17 @@
 package src.use_case.preferences;
 
 public class PreferencesOutputData {
+    private final String dietaryChoice;
 
+    private final String allergiesChoice;
+    private final String conditionsChoice;
+
+    public PreferencesOutputData(String dietaryChoice, String allergiesChoice, String conditionsChoice){
+        this.dietaryChoice = dietaryChoice;
+        this.allergiesChoice = allergiesChoice;
+        this.conditionsChoice = conditionsChoice;
+    }
+    public String getDietaryChoice(){return dietaryChoice;}
+    public String getAllergiesChoice(){return allergiesChoice;}
+    public String getConditionsChoice(){return conditionsChoice;}
 }
