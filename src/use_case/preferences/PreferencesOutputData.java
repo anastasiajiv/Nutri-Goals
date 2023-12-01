80ef1403b93ea8557c4b74a4f69accfafb56db4d
Anastasia <anastasiajiv13@gmail.com>
2023-12-01 14:14:54 -0500

    changed the type for the attributes in output data class

--- src/use_case/preferences/PreferencesOutputData.java
+++ src/use_case/preferences/PreferencesOutputData.java
@@ -1,17 +1,20 @@
 package src.use_case.preferences;
 
+import java.util.*;
 public class PreferencesOutputData {
-    private final String dietaryChoice;
+    private final HashMap<String, Boolean> dietaryChoice;
 
-    private final String allergiesChoice;
-    private final String conditionsChoice;
+    private final HashMap<String, Boolean> allergiesChoice;
+    private final HashMap<String, String> conditionsChoice;
 
-    public PreferencesOutputData(String dietaryChoice, String allergiesChoice, String conditionsChoice){
+    public PreferencesOutputData(HashMap<String, Boolean> dietaryChoice,
+                                 HashMap<String, Boolean> allergiesChoice,
+                                 HashMap<String, String> conditionsChoice){
         this.dietaryChoice = dietaryChoice;
         this.allergiesChoice = allergiesChoice;
         this.conditionsChoice = conditionsChoice;
     }
-    public String getDietaryChoice(){return dietaryChoice;}
-    public String getAllergiesChoice(){return allergiesChoice;}
-    public String getConditionsChoice(){return conditionsChoice;}
+    public HashMap<String, Boolean> getDietaryChoice(){return dietaryChoice;}
+    public HashMap<String, Boolean> getAllergiesChoice(){return allergiesChoice;}
+    public HashMap<String, String> getConditionsChoice(){return conditionsChoice;}
 }
