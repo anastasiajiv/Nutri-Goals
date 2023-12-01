235341013a31db227781c1c5004e7154ed8837a7
Anastasia <anastasiajiv13@gmail.com>
2023-12-01 14:10:18 -0500

    added setter methods

--- src/interface_adapters/preferences/PreferencesState.java
+++ src/interface_adapters/preferences/PreferencesState.java
@@ -13,8 +13,12 @@ public class PreferencesState {
 
     public PreferencesState(){}
 
+    public void setDietaryMap(HashMap<String, Boolean> dietaryMap){this.dietary = dietaryMap;}
     public HashMap<String, Boolean> getDietaryMap(){return dietary;}
+
+    public void setAllergiesMap(HashMap<String, Boolean> allergiesMap){this.allergies = allergiesMap;}
     public HashMap<String, Boolean> getAllergiesMap(){return allergies;}
+    public void setConditionsMap(HashMap<String, String> conditionsMap){this.conditions =conditionsMap;}
     public HashMap<String, String> getConditionsMap(){return conditions;}
 
 }
