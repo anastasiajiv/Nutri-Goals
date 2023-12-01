4e41b6a23fed062b40844f50c703761f9f8f79b1
Anastasia <anastasiajiv13@gmail.com>
2023-12-01 00:07:51 -0500

    added button attributes for dietary options

---
+++ src/view/PreferencesView.java
@@ -0,0 +1,15 @@
+package src.view;
+
+import javax.swing.*;
+
+public class PreferencesView {
+    public final String viewName = "preferences";
+
+    private final JRadioButton vegan;
+
+    private final JRadioButton vegetarian;
+
+    private final JRadioButton pescetarian;
+
+
+}
