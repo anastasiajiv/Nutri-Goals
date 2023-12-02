fa3454f6df4fdde2f8d177caebfeca215f189027
Anastasia <anastasiajiv13@gmail.com>
2023-12-02 01:55:34 -0500

    changes were made to account for the creation of UserID

--- src/use_case/signup/SignupInteractor.java
+++ src/use_case/signup/SignupInteractor.java
@@ -1,10 +1,44 @@
 package src.use_case.signup;
 
-
+import src.entity.User;
+import src.entity.UserFactory;
 
 import java.time.LocalDateTime;
 
-public class SignupInteractor {
+public class SignupInteractor implements SignupInputBoundary{
+    final SignupUserDataAccessInterface userDataAccessObject;
+    final SignupOutputBoundary userPresenter;
+    final UserFactory userFactory;
 
+    public SignupInteractor(SignupUserDataAccessInterface signupDataAccessInterface,
+                            SignupOutputBoundary signupOutputBoundary,
+                            UserFactory userFactory) {
+        this.userDataAccessObject = signupDataAccessInterface;
+        this.userPresenter = signupOutputBoundary;
+        this.userFactory = userFactory;
+    }
+
+    @Override
+    public void execute(SignupInputData signupInputData) {
+        int createdUserID = userDataAccessObject.createUserID();
+        LocalDateTime creationTime = LocalDateTime.now();
+        if (userDataAccessObject.existByUserID(createdUserID)) { //
+            // check in case user already exists in accounts map.
+            userPresenter.prepareFailView("This user already exits");// should be impossible
+        } else if (!signupInputData.getPassword().equals(signupInputData.getRepeatPassword())) {
+            userPresenter.prepareFailView("Passwords do not match");
+        } else {
+
+            userDataAccessObject.saveUserSignUpData(createdUserID,
+                    signupInputData.getUsername(),
+                    signupInputData.getPassword(),
+                    creationTime);
+
+            SignupOutputData signupOutputData = new SignupOutputData(createdUserID,
+                    signupInputData.getUsername(),
+                    creationTime.toString(), false);
+            userPresenter.prepareSuccessView(signupOutputData);
+        }
+    }
 
 }// GIT WORKFLOW
