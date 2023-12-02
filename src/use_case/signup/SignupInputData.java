3c1f33a815b30020b15071e6e428da41b4d77c84
Anastasia <anastasiajiv13@gmail.com>
2023-12-02 01:58:29 -0500

    changes were made to account for the creation of userID for each new user

--- src/use_case/signup/SignupInputData.java
+++ src/use_case/signup/SignupInputData.java
@@ -2,5 +2,29 @@ package src.use_case.signup;
 
 public class SignupInputData {
 
+    //final private int userID;
+    final private String username;
+    final private String password;
+    final private String repeatPassword;
 
+    public SignupInputData(String username, String password, String repeatPassword) {
+        //this.userID = userID;
+        this.username = username;
+        this.password = password;
+        this.repeatPassword = repeatPassword;
+    }
+
+    //int getUserID(){return userID;}
+
+    String getUsername() {
+        return username;
+    }
+
+    String getPassword() {
+        return password;
+    }
+
+    public String getRepeatPassword() {
+        return repeatPassword;
+    }
 }
--- src/use_case/signup/SignupOutputData.java
+++ src/use_case/signup/SignupOutputData.java
@@ -1,7 +1,37 @@
 package src.use_case.signup;
 
 public class SignupOutputData {
+    private  int createdUserID;
 
+    private String username;
+
+    private String creationTime;
+
+    private boolean useCaseFailed;
+
+
+    public SignupOutputData(int createdUserID, String username, String creationTime, boolean useCaseFailed) {
+        this.createdUserID = createdUserID;
+        this.username = username;
+        this.creationTime = creationTime;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public int getCreationUserID() {
+        return createdUserID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(String creationTime) {
+        this.creationTime = creationTime;
+    }
 
 
 }
