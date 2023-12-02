64a42d231ead768f5f17afaf7c3932b95db36834
Anastasia <anastasiajiv13@gmail.com>
2023-12-02 01:50:54 -0500

    made changes to account for UserID but due to error these have been commented out

--- src/view/LoggedInView.java
+++ src/view/LoggedInView.java
@@ -1,4 +1,65 @@
 package src.view;
 
-public class LoggedInView {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import src.interface_adapters.logged_in.LoggedInState;
+import src.interface_adapters.logged_in.LoggedInViewModel;
+
+
+public class LoggedInView extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "logged in";
+    private final LoggedInViewModel loggedInViewModel;
+
+    JLabel username;
+    JLabel userID;
+
+    final JButton logOut;
+
+    /**
+     * A window with a title and a JButton.
+     */
+    public LoggedInView(LoggedInViewModel loggedInViewModel) {
+        this.loggedInViewModel = loggedInViewModel;
+        this.loggedInViewModel.addPropertyChangeListener(this);
+
+        JLabel title = new JLabel("Logged In Screen");
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel usernameInfo = new JLabel("Currently logged in: ");
+        username = new JLabel();
+        userID = new JLabel();
+
+        JPanel buttons = new JPanel();
+        logOut = new JButton(loggedInViewModel.LOGOUT_BUTTON_LABEL);
+        buttons.add(logOut);
+
+        logOut.addActionListener(this);
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.add(title);
+        this.add(usernameInfo);
+        this.add(username);
+        this.add(userID);
+        this.add(buttons);
+    }
+
+    /**
+     * React to a button click that results in evt.
+     */
+    public void actionPerformed(ActionEvent evt) {
+        System.out.println("Click " + evt.getActionCommand());
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        LoggedInState state = (LoggedInState) evt.getNewValue();
+        username.setText(state.getUsername());
+        //userID.setText(Integer.toString(state.getUserID()));
+    }
 }
