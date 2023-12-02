933e8840e247ba1942978d93370135a213c32c30
Anastasia <anastasiajiv13@gmail.com>
2023-12-02 02:11:42 -0500

    changes were made in login to account for userID however, have since these changes have been commented out

--- src/use_case/login/LoginInputData.java
+++ src/use_case/login/LoginInputData.java
@@ -1,6 +1,24 @@
 package src.use_case.login;
 
 public class LoginInputData {
+    //final private int userID;
+    final private String username;
+    final private String password;
 
+    public LoginInputData( String username, String password) {
+        //this.userID = userID;
+        this.username = username;
+        this.password = password;
+    }
+
+    //int getUserID(){return userID;}
+
+    String getUsername() {
+        return username;
+    }
+
+    String getPassword() {
+        return password;
+    }
 
 }
