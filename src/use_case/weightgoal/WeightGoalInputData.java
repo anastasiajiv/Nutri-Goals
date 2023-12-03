7fa04700db95c3af1992715487add70cae233149
Anastasia <anastasiajiv13@gmail.com>
2023-12-03 01:23:43 -0500

    updated weightgoal input data

--- src/use_case/weightgoal/WeightGoalInputData.java
+++ src/use_case/weightgoal/WeightGoalInputData.java
@@ -7,27 +7,69 @@ import src.entity.User;
 import src.entity.CommonUser;
 public class WeightGoalInputData {
 
-    private HashMap<Integer, String> weightGoalType;
+    private final int userId;
+
+    private HashMap<String, Boolean> gender;
+
+    private double height;
+
+    private double weight;
+
+    private int age;
+
+    private int exerciseLvl;
+
+    private String paceType;
 
 
-    public WeightGoalInputData(HashMap<Integer, String> weightGoalType){
-        this.weightGoalType = weightGoalType;
+
+    private final HashMap<String, Boolean> weightGoal; // Hashmap<maintainWeight, False>  for all weightGoalTypes
+
+
+
+
+    public WeightGoalInputData(int userId,
+                               HashMap<String, Boolean> gender,
+                               double height,
+                               double weight,
+                               int age,
+                               int exerciseLvl,
+                               String paceType,
+                               HashMap<String, Boolean> weightGoal){
+        this.userId = userId;
+        this.weightGoal = weightGoal;
     }
 
-    String getWeightGoalType(){
-        String strWeightGoalType = "";
-        for (Map.Entry<Integer, String> entry: weightGoalType.entrySet()) { // Should only be 1 loop
-            strWeightGoalType = entry.getValue();
-        }
-        return strWeightGoalType;
-
+    HashMap<String, Boolean> getGender() {
+        return this.gender;
     }
+
+    double getWeight() {
+        return this.weight;
+    }
+
+    double getHeight() {
+        return height;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    int getExerciseLvl() {
+        return exerciseLvl;
+    }
+
+    String getPaceType() {
+        return paceType;
+    }
+
+    HashMap<String, Boolean> getWeightGoal(){
+        return weightGoal;
+    }
+
 
     int getUserId(){
-        int userId = 0;
-        for (Map.Entry<Integer, String> entry: weightGoalType.entrySet()) { // Should only be 1 loop
-            userId = entry.getKey();
-        }
         return userId;
     }
 }
