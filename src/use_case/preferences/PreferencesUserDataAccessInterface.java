45724f54ad180bc94003bd9c728286b22132e435
Anastasia <anastasiajiv13@gmail.com>
2023-12-02 22:50:01 -0500

    added exist by UserId and getAccountByUserID and now there is a common save preference method

--- src/use_case/preferences/PreferencesUserDataAccessInterface.java
+++ src/use_case/preferences/PreferencesUserDataAccessInterface.java
@@ -1,12 +1,20 @@
 package src.use_case.preferences;
+import src.entity.User;
+
 import java.util.*;
 public interface PreferencesUserDataAccessInterface {
     // where for each Integer refers to userId
     // want to change the way that the parameters for each method, UserID(which is an integer should be seperate)
-    void saveDietary(HashMap<Integer, HashMap<String, Boolean>> user_dietery);
-    void saveConditions(HashMap<Integer, HashMap<String, String>> user_condition);
+    //void saveDietary(HashMap<Integer, HashMap<String, Boolean>> user_dietery);
+    //void saveConditions(HashMap<Integer, HashMap<String, String>> user_condition);
 
-    void saveAllergies(HashMap<Integer, HashMap<String, Boolean>> user_allergies);
+    //void saveAllergies(HashMap<Integer, HashMap<String, Boolean>> user_allergies);
 
-    boolean existById(int id);
+    Boolean savePreferences(int userId, HashMap<String, Boolean> user_dietery,
+                         HashMap<String, Boolean> user_allergies,
+                         HashMap<String, String> user_condition);
+
+    Boolean existByUserID(int userID);
+
+    public User getAccountByUserId (int userId);
 }
