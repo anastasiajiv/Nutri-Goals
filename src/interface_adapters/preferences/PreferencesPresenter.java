cd5e11390de1ec99e6e04445c49f9f58e6757f98
Anastasia <anastasiajiv13@gmail.com>
2023-12-02 22:35:20 -0500

    the prepare successview and the preparefail view were implemented

--- src/interface_adapters/preferences/PreferencesPresenter.java
+++ src/interface_adapters/preferences/PreferencesPresenter.java
@@ -2,15 +2,38 @@ package src.interface_adapters.preferences;
 
 import src.interface_adapters.ViewManagerModel;
 import src.use_case.preferences.PreferencesOutputBoundary;
-
-public class PreferencesPresenter implements PreferencesOutputBoundary {
+import src.use_case.preferences.PreferencesOutputData;
+import src.interface_adapters.logged_in.LoggedInState;
+import src.interface_adapters.logged_in.LoggedInViewModel;
+public class PreferencesPresenter implements PreferencesOutputBoundary{
     private final PreferencesViewModel preferencesViewModel;
+    private final LoggedInViewModel loggedInViewModel;
 
     private ViewManagerModel viewManagerModel;
 
-    public PreferencesPresenter(ViewManagerModel viewManagerModel, PreferencesViewModel preferencesViewModel){
+    public PreferencesPresenter(ViewManagerModel viewManagerModel, PreferencesViewModel preferencesViewModel,
+                                LoggedInViewModel loggedInViewModel){
         this.preferencesViewModel = preferencesViewModel;
         this.viewManagerModel = viewManagerModel;
+        this.loggedInViewModel = loggedInViewModel;
 
     }
+
+    @Override
+    public void prepareSuccessView(PreferencesOutputData response){
+        LoggedInState loggedInState = loggedInViewModel.getState();
+        this.loggedInViewModel.setState(loggedInState);
+        this.loggedInViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setActiveView(loggedInViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error){
+        PreferencesState preferencesState = preferencesViewModel.getState();
+        preferencesState.setPreferencesError(error);
+        preferencesViewModel.firePropertyChanged();
+    }
+
 }
