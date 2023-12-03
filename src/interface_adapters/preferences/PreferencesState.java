b56d700509bc9728df9081f3d0cefe8290af2fb4
Anastasia <anastasiajiv13@gmail.com>
2023-12-02 22:36:33 -0500

    created attributes and setter and getter methods in the state

--- src/interface_adapters/preferences/PreferencesState.java
+++ src/interface_adapters/preferences/PreferencesState.java
@@ -1,17 +1,23 @@
 package src.interface_adapters.preferences;
 import java.util.*;
 public class PreferencesState {
+    private int userID;
     private HashMap<String, Boolean> dietary = null;
     private HashMap<String, Boolean> allergies = null;
     private HashMap<String, String> conditions = null;
-
+    private String preferencesError;
     public PreferencesState(PreferencesState copy){
+        userID = copy.userID;
         dietary = copy.dietary;
         allergies = copy.allergies;
         conditions = copy.conditions;
+        preferencesError = copy.preferencesError;
     }
 
     public PreferencesState(){}
+
+    public void setUserID(int userID) {this.userID = userID;}
+    public int getUserID(){return userID;}
 
     public void setDietaryMap(HashMap<String, Boolean> dietaryMap){this.dietary = dietaryMap;}
     public HashMap<String, Boolean> getDietaryMap(){return dietary;}
@@ -20,5 +26,5 @@ public class PreferencesState {
     public HashMap<String, Boolean> getAllergiesMap(){return allergies;}
     public void setConditionsMap(HashMap<String, String> conditionsMap){this.conditions =conditionsMap;}
     public HashMap<String, String> getConditionsMap(){return conditions;}
-
+    public String setPreferencesError(String preferencesError){return "Unable to set preferences. Please try again.";}
 }
