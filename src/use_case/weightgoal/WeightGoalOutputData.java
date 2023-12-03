fc1f7c7c6034af3d8cf6c0c64fe859ae8210b80e
Anastasia <anastasiajiv13@gmail.com>
2023-12-03 01:25:51 -0500

    updated the output data class to reflect recent changes

--- src/use_case/weightgoal/WeightGoalOutputData.java
+++ src/use_case/weightgoal/WeightGoalOutputData.java
@@ -1,4 +1,15 @@
 package src.use_case.weightgoal;
 
 public class WeightGoalOutputData {
+    private final int userID;
+    private boolean useCaseFailed;
+
+    public WeightGoalOutputData(int userID, boolean useCaseFailed) {
+        this.userID = userID;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public int getUserID() {
+        return this.userID;
+    }
 }
