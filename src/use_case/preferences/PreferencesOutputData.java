d12f53628232739682efc84f5fb4d5f24d0de085
Anastasia <anastasiajiv13@gmail.com>
2023-12-02 22:50:53 -0500

    the output should now be the userID appropriate changes were made for this

--- src/use_case/preferences/PreferencesOutputData.java
+++ src/use_case/preferences/PreferencesOutputData.java
@@ -2,19 +2,10 @@ package src.use_case.preferences;
 
 import java.util.*;
 public class PreferencesOutputData {
-    private final HashMap<String, Boolean> dietaryChoice;
+    private final int userID;
 
-    private final HashMap<String, Boolean> allergiesChoice;
-    private final HashMap<String, String> conditionsChoice;
-
-    public PreferencesOutputData(HashMap<String, Boolean> dietaryChoice,
-                                 HashMap<String, Boolean> allergiesChoice,
-                                 HashMap<String, String> conditionsChoice){
-        this.dietaryChoice = dietaryChoice;
-        this.allergiesChoice = allergiesChoice;
-        this.conditionsChoice = conditionsChoice;
+    public PreferencesOutputData(int userID){
+        this.userID = userID;
     }
-    public HashMap<String, Boolean> getDietaryChoice(){return dietaryChoice;}
-    public HashMap<String, Boolean> getAllergiesChoice(){return allergiesChoice;}
-    public HashMap<String, String> getConditionsChoice(){return conditionsChoice;}
+    public int getID(){return this.userID;}
 }
