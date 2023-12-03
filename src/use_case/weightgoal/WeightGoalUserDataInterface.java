9b9a104e5c456bf716aa43b2e41e9255cba80bb8
Anastasia <anastasiajiv13@gmail.com>
2023-12-03 01:19:14 -0500

    commented out some methods

--- src/use_case/weightgoal/WeightGoalUserDataInterface.java
+++ src/use_case/weightgoal/WeightGoalUserDataInterface.java
@@ -1,15 +1,28 @@
 package src.use_case.weightgoal;
 
 import src.entity.User;
-
+import java.util.*;
 public interface WeightGoalUserDataInterface {
-    void saveWeightGoalData(User user);
+    Boolean saveWeightGoalData(int userId,
+                               HashMap<String, Boolean> gender,
+                               double height,
+                               double weight,
+                               int age,
+                               int exerciseLvl,
+                               String paceType,
+                               HashMap<String, Boolean> weightGoal);
 
-    User getUserWeightGoalData(int userId);
+    double computedRequiredCalories (int userId) ;
 
-    Boolean existByUserId(int userId);
+    Boolean existByUserID(int userID);
 
-    String getWeightGoalType(int userId);
+    public User getAccountByUserId ( int userId);
+
+    //User getUserWeightGoalData(int userId);
+
+    //Boolean existByUserId(int userId);
+
+    //String getWeightGoalType(int userId);
 
 }
 
