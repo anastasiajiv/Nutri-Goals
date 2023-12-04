5bd6465443b43bf52ef52febdf02020d6857b1af
esha <eshh.d0@gmail.com>
2023-12-04 17:19:35 -0500

    Fixed import error in MealPlanPresenter

--- src/interface_adapters/mealplan/MealPlanPresenter.java
+++ src/interface_adapters/mealplan/MealPlanPresenter.java
@@ -1,11 +1,12 @@
 package src.interface_adapters.mealplan;
 
 import src.use_case.mealplan.MealPlanOutputBoundary;
+import src.interface_adapters.ViewManagerModel;
 import src.use_case.mealplan.MealPlanOutputData;
 
 public class MealPlanPresenter implements MealPlanOutputBoundary {
     private final MealPlanViewModel mealPlanViewModel;
-    private ViewManagermodel viewManagermodel;
+    private ViewManagerModel viewManagermodel;
 
     public MealPlanPresenter(MealPlanViewModel mealPlanViewModel){
         this.mealPlanViewModel = mealPlanViewModel;
