7f58ca99132125439f2439ed8bc3d42ea29a6bab
Aarya Bhardawaj <aarya.bhardawaj@gmail.com>
2023-12-04 03:22:26 -0500

    implemented create method to import into usecase factory

--- src/view/WeightGoalsView.java
+++ src/view/WeightGoalsView.java
@@ -1,4 +1,18 @@
 package src.view;
 
+import src.interface_adapters.weightgoal.WeightGoalController;
+import src.interface_adapters.weightgoal.WeightGoalViewModel;
+
 public class WeightGoalsView {
+
+    private final WeightGoalViewModel weightGoalViewModel;
+    private final WeightGoalController weightGoalController;
+
+
+    public WeightGoalsView(WeightGoalController weightGoalController,
+                           WeightGoalViewModel weightGoalViewModel) {
+
+        this.weightGoalViewModel = weightGoalViewModel;
+        this.weightGoalController = weightGoalController;
+    }
 }
